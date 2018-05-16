48d17d2325ba3934c38c5c40c039dce5912f7e91
Sahapat <sahapat.starcaft@gmail.com>
2018-05-16 19:03:51 +0700

    Init Project

---
+++ Assets/Scripts/InteracableBananaHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+using UnityEngine.UI;
+
+public class InteracableBananaHandler : MonoBehaviour
+{
+    [SerializeField] private VRInteractiveItem m_InteractiveItem;
+    [SerializeField] private VRInput m_VRInput;
+    [SerializeField] private Text text;
+    private bool m_GazeOver;
+
+    private AudioSource audioSource;
+    private MeshRenderer meshRenderer;
+
+    [SerializeField] private float respawnTime;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    private void OnEnable()
+    {
+        m_VRInput.OnDown += HandleDown;
+        m_VRInput.OnUp += HandleUp;
+
+        m_InteractiveItem.OnOver += HandleOver;
+        m_InteractiveItem.OnOut += HandleOut;
+    }
+
+    private void OnDisable()
+    {
+        m_VRInput.OnDown -= HandleDown;
+        m_VRInput.OnUp -= HandleUp;
+
+        m_InteractiveItem.OnOver -= HandleOver;
+        m_InteractiveItem.OnOut -= HandleOut;
+    }
+    private void HandleOut()
+    {
+        m_GazeOver = false;
+    }
+
+    private void HandleOver()
+    {
+        m_GazeOver = true;
+    }
+
+    private void HandleUp()
+    {
+        text.text = "Up";
+        text.color = Color.red;
+    }
+
+    private void HandleDown()
+    {
+        /*if(m_GazeOver)
+        {
+            audioSource.Play();
+            meshRenderer.enabled = false;
+
+            Invoke("RespawnObject", respawnTime);
+        }*/
+        text.text = "Down";
+        text.color = Color.green;
+    }
+    private void RespawnObject()
+    {
+        meshRenderer.enabled = true;
+        CancelInvoke();
+    }
+}
---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private float yFloating;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Boat"))
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.position = new Vector3(other.transform.position.x, yFloating, other.transform.position.z);
+        }
+    }
+}
